027a5041f4f6c23b564c34a1bf6fb857a493ecbf
HuseynovaDilara <huseynova.8885@gmail.com>
2024-06-01 16:18:52 +0400

    BubleSortAdded

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Sorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 9, 5, 6, 3, 7, 2 };
+            BubleSort(arr);
+
+        }
+        public static void BubleSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            foreach (int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+        }
+            
+    }
+}
